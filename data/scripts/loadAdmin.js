c3e46a41c832cebd4a9eb620607bec23e5cdd089
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-03-07 17:29:53 +0000

    Update loadAdmin.js

--- data/scripts/loadAdmin.js
+++ data/scripts/loadAdmin.js
@@ -1,7 +1,7 @@
 //create variables
 //var textarea = document.getElementById('textcenter'); var hasLoggedInBefore = localStorage.getItem('lqowgegretdyiqnwbeerftoyrqe'); 
 var nextUrl = 0;
-nextUrl = 'https://www.piggame.co.uk/admin/login?source=norm';
+nextUrl = '/login';
 
 //Change the URL
-window.location.href = nextUrl;
+window.location.replace(nextUrl);
