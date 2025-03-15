3c55311f535a7e90c6a539d78d28f63d32c5150f
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-03-15 17:14:16 +0000

    Create theShopHomePageScript.js

---
+++ data/scripts/theShopHomePageScript.js
@@ -0,0 +1,7 @@
+const productsContainer = document.getElementsByClassName("productscontainer")[0];
+const productsContainerLen = productsContainer.length
+
+for (let x = 0; x < productsContainerLen; x++) {
+  z = productsContainer.getElementsByClassName("productcontainer")[x].id
+  document.getElementById('productImage_' + z).src='/data/images/the_shop_' + z + '_product-image_1';
+}
