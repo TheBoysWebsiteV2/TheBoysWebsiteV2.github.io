7a42a6a942dba6794aaa407cbe286253442c33bf
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-03-16 17:24:34 +0000

    Update adminData.js

--- data/scripts/adminData.js
+++ data/scripts/adminData.js
@@ -10,7 +10,7 @@ var ranks = {
     'VBurden': 'Standard',
     'TGreen': 'Standard',
     'DKennedy': 'Standard',
-    'OVermunt': 'Admin',
+    'OVermunt': 'Full',
     'LKane': 'Standard'
 };
 var lockedAccounts = {
