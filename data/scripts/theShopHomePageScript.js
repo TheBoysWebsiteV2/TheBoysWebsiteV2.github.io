682dade607702f974147a57681483e68dae8fe16
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-03-16 16:49:26 +0000

    Update theShopHomePageScript.js

--- data/scripts/theShopHomePageScript.js
+++ data/scripts/theShopHomePageScript.js
@@ -1,8 +1,15 @@
-const productsContainer = document.getElementsByClassName("productscontainer")[0];
-const productContainers = productsContainer.getElementsByClassName("productcontainer");
-const productsContainerLen = productContainers.length;
+document.addEventListener('DOMContentLoaded', function() {
+    const productsContainer = document.getElementsByClassName("productscontainer")[0];
+    
+    if (productsContainer) { // Check if productsContainer is not undefined
+        const productContainers = productsContainer.getElementsByClassName("productcontainer");
+        const productsContainerLen = productContainers.length;
 
-for (let x = 0; x < productsContainerLen; x++) {
-  let z = productContainers[x].id;
-  document.getElementById('productImage_' + z).src='/data/images/the_shop_' + z + '_product-image_1';
-}
+        for (let x = 0; x < productsContainerLen; x++) {
+            let z = productContainers[x].id;
+            document.getElementById('productImage_' + z).src = '/data/images/the_shop_' + z + '_product-image_1';
+        }
+    } else {
+        console.error("No elements with class 'productscontainer' found.");
+    }
+});
