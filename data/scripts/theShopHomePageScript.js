c4d413e6cc40cdd5689b6bf062d589ed1477ed70
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-03-16 17:07:26 +0000

    Update theShopHomePageScript.js

--- data/scripts/theShopHomePageScript.js
+++ data/scripts/theShopHomePageScript.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         for (let x = 0; x < productsContainerLen; x++) {
             let z = productContainers[x].id;
             document.getElementById('productImage_' + z).src = '/data/images/the_shop_' + z + '_product-image_1.png';
+            document.getElementById('productTitle_' + z).innerHTML = theShopItemNames[567394];
         }
     } else {
         console.error("No elements with class 'productscontainer' found.");
