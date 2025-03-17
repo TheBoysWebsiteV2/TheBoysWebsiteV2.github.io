e75115a44358ac04dc19c28dba27f076d0b84d0b
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-03-17 17:53:23 +0000

    Update theShopProductPageScript.js

--- data/scripts/theShopProductPageScript.js
+++ data/scripts/theShopProductPageScript.js
@@ -1,6 +1,14 @@
-const urlParams = new URLSearchParams(window.location.search); const z = urlParams.get('code'); 
-document.addEventListener('DOMContentLoaded', function() {
+const urlParams = new URLSearchParams(window.location.search); const z = urlParams.get('code');
+document.addEventListener('DOMContentLoaded', function() { 
   document.getElementById('productImage' + z).src = '/data/images/the_shop_' + z + '_product-image_1.png';
   document.getElementById('productTitle' + z).innerHTML = theShopItemNames[z];
   document.getElementById('productPrice' + z).innerHTML = theShopItemPrices[z];
 });
+
+function addToCart {
+  const b = sessionStorage.getItem('theShopCart');
+  if (b == null) {
+    sessionStorage.setItem('theShopCart', z);
+  } else {
+    const g = b + ' ' + z
+    sessionStorage.setItem('theShopCart', g);
