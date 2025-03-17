c22e94871639ce3339a97ec1ebafc967cf56b2c8
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-03-17 18:38:16 +0000

    Update theShopProductPageScript.js

--- data/scripts/theShopProductPageScript.js
+++ data/scripts/theShopProductPageScript.js
@@ -1,6 +1,6 @@
-const urlParams = new URLSearchParams(window.location.search); const z = urlParams.get('code');
+const urlParams = new URLSearchParams(window.location.search); const bgh = urlParams.get('code');
 document.addEventListener('DOMContentLoaded', function() { 
-  document.getElementById('productImage' + z).src = '/data/images/the_shop_' + z + '_product-image_1.png';
-  document.getElementById('productTitle' + z).innerHTML = theShopItemNames[z];
-  document.getElementById('productPrice' + z).innerHTML = theShopItemPrices[z];
+  document.getElementById('productImage' + bgh).src = '/data/images/the_shop_' + bgh + '_product-image_1.png';
+  document.getElementById('productTitle' + bgh).innerHTML = theShopItemNames[bgh];
+  document.getElementById('productPrice' + bgh).innerHTML = theShopItemPrices[bgh];
 });
