392be9524642ac4203d0184ff8ec8549a708e349
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-03-21 16:43:24 +0000

    Update theShopCartPageScript.js

--- data/scripts/theShopCartPageScript.js
+++ data/scripts/theShopCartPageScript.js
@@ -1,12 +1,15 @@
 //TO BE CONTINUED!!!
 const cart = sessionStorage.getItem('theShopCart'); let cartItemsDiv = document.getElementById('cartItemsDiv');
 if (cart == null) {
-  cartItemsDiv.innerHTML='Your cart is empty.';
+  cartItemsDiv.innerHTML='<h3>Your cart is empty.</h3>';
 } else {
   const cartItemsLen = cart.length;
   const splitCart = cart.split(' ');
   for (let x = 0; x < cartItemsLen; x++) {
+    const currentItemCode = splitCart[x];
     const itemElement = document.createElement('div');
-    itemElement.class = 'productContainer');
+    itemElement.class = 'productContainer';
+    itemElement.innerHTML = '<p><img src="' + currentItemCode + '"/></p><p><strong>' + theShopItemNames[currentItemCode] + '</strong></p><br><p>' + theShopItemPrices[currentItemCode] + '</p>';
+    cartItemsDiv.appendChild(itemElement);
   };
 };
