6839b839b8c538b2be68ee41eba73ae8e4d8238d
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-03-22 10:18:52 +0000

    Update theShopCartPageScript.js

--- data/scripts/theShopCartPageScript.js
+++ data/scripts/theShopCartPageScript.js
@@ -1,17 +1,47 @@
 //TO BE CONTINUED!!!
-const cart = sessionStorage.getItem('theShopCart'); let cartItemsDiv = document.getElementById('cartItemsDiv');
+//const cart = sessionStorage.getItem('theShopCart'); let cartItemsDiv = document.getElementById('cartItemsDiv');
+//if (cart == null) {
+//  cartItemsDiv.innerHTML='<h3>Your cart is empty.</h3>';
+//} else {
+//  const cartItemsLen = cart.length;
+//  const splitCart = cart.split(' ');
+//  console.log(splitCart);
+//  for (let x = 0; x < cartItemsLen; x++) {
+//    const currentItemCode = splitCart[x];
+//    console.log(currentItemCode);
+//    const itemElement = document.createElement('div');
+//    itemElement.setAttribute('class', 'productcontainer');
+//    itemElement.innerHTML = '<p><img src="/data/images/the_shop_' + currentItemCode + '_product-image_1.png"/></p><p><strong>' + theShopItemNames[currentItemCode] + '</strong></p><br><p>' + theShopItemPrices[currentItemCode] + '</p>';
+//    cartItemsDiv.appendChild(itemElement);
+//  };
+//};
+//
+// Ensure `theShopItemNames` and `theShopItemPrices` are defined or imported
+const cart = sessionStorage.getItem('theShopCart'); 
+let cartItemsDiv = document.getElementById('cartItemsDiv');
+
 if (cart == null) {
-  cartItemsDiv.innerHTML='<h3>Your cart is empty.</h3>';
+  cartItemsDiv.innerHTML = '<h3>Your cart is empty.</h3>';
 } else {
-  const cartItemsLen = cart.length;
   const splitCart = cart.split(' ');
   console.log(splitCart);
-  for (let x = 0; x < cartItemsLen; x++) {
+  
+  for (let x = 0; x < splitCart.length; x++) {
     const currentItemCode = splitCart[x];
     console.log(currentItemCode);
-    const itemElement = document.createElement('div');
-    itemElement.setAttribute('class', 'productcontainer');
-    itemElement.innerHTML = '<p><img src="/data/images/the_shop_' + currentItemCode + '_product-image_1.png"/></p><p><strong>' + theShopItemNames[currentItemCode] + '</strong></p><br><p>' + theShopItemPrices[currentItemCode] + '</p>';
-    cartItemsDiv.appendChild(itemElement);
-  };
-};
+    
+    // Check if `currentItemCode` is valid and exists in `theShopItemNames`
+    if (theShopItemNames[currentItemCode]) {
+      const itemElement = document.createElement('div');
+      itemElement.setAttribute('class', 'productcontainer');
+      itemElement.innerHTML = `
+        <p><img src="/data/images/the_shop_${currentItemCode}_product-image_1.png"/></p>
+        <p><strong>${theShopItemNames[currentItemCode]}</strong></p>
+        <br>
+        <p>${theShopItemPrices[currentItemCode]}</p>`;
+      cartItemsDiv.appendChild(itemElement);
+    } else {
+      console.warn(`Item code ${currentItemCode} not found in theShopItemNames`);
+    }
+  }
+}
