d3e64677670652515b39d23a824dee2aae906811
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-06-21 09:02:23 +0100

    Update adminLoginService.js

--- data/scripts/adminLoginService.js
+++ data/scripts/adminLoginService.js
@@ -3,22 +3,36 @@
 */
 
 //ϟcreate variablesϟ
-const urlParams = new URLSearchParams(window.location.search); const username = urlParams.get('username'); const password = urlParams.get('password');
+const urlParams = new URLSearchParams(window.location.search); const username = urlParams.get('username'); const password = urlParams.get('password'); const accountIDforQR = urlParams.get('accountID');
 /*var z = Math.floor(Math.random() * 1000000000000);
 document.write("Session ID: " + z);
 sessionStorage.setItem('session-id', z);*/
+//hack prevention
+console.info('ϟSCRIPT-LOAD: adminSecurityCheck.js [SUCCESFUL]');
+var ca = sessionStorage.getItem('currentAccount'); var referrer = document.referrer;
+if (referrer == '') {
+  window.location.replace('/login?source=loginsericewrongurl');
+} else if (referrer) {
+  if (ca == null) {
+    //continue
+  } else {
+    window.location.replace('/login?source=loginsericeusernameinsessionstorage');
+  }
+}
+
+console.info('Hack checks did not detect any hacks.');
 
 if (username in data) {
     if (data[username] === password) {
         sessionStorage.setItem('currentAccount', username);
         sessionStorage.setItem('userRank', ranks[username]);
     } else {
-        window.location.replace('/login');
+        window.location.replace('/login?source=passwordWrong');
     }
 } else {
-    window.location.replace('/login');
+    window.location.replace('/login?source=accountNotFound');
 }
 
 //Head to admin home screen
 
-window.location.replace('/dashboard/home');
+window.location.replace('/dashboard');
