b54dcd9df8acabfd53372d7db7720d26816c123a
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-11-01 17:49:43 +0000

    Refactor product container handling in script

--- data/scripts/theShopHomePageScript.js
+++ data/scripts/theShopHomePageScript.js
@@ -62,52 +62,54 @@ document.addEventListener('DOMContentLoaded', function() {
 
 let loadsecond = false;
 document.addEventListener('DOMContentLoaded', function() {
-    const productsContainer = document.getElementsByClassName("productscontainer")[0];
-
+    const productsContainers = document.getElementsByClassName("productscontainer");
+    const productsContainersNo = productsContainers.length
     if (productsContainer) { // Check if productsContainer is not undefined
-        const productContainers = productsContainer.getElementsByClassName("productcontainer");
-        const productsContainerLen = productContainers.length;
-        console.log('PRODUCT CONTAINER LEN: ' + productsContainerLen)
-        for (let x = 0; x < productsContainerLen; x++) {
-            let z = productContainers[x].id;
-            console.log(z)
+        for (const productsContainer of productsContainers) {
+            const productContainers = productsContainer.getElementsByClassName("productcontainer");
+            const productsContainerLen = productContainers.length;
+            console.log('PRODUCT CONTAINER LEN: ' + productsContainerLen)
+            for (let x = 0; x < productsContainerLen; x++) {
+                let z = productContainers[x].id;
+                console.log(z)
 
-            // Create elements
-            let center = document.createElement('center')
+                // Create elements
+                let center = document.createElement('center')
             
-            let img = document.createElement('img')
-            img.src = '/data/images/the_shop_' + z + '_product-image_1.png';
-
-            let title = document.createElement('p')
-            title.style.fontWeight = 'bold';
-            title.innerHTML = theShopItemNames[z];
-
-            let price = document.createElement('p')
-            price.style.margin = '0 auto';
-            price.innerHTML = '£' + theShopItemPrices[z];
-            
-            // Append elements to the container
-            center.appendChild(img)
-            productContainers[x].appendChild(center);
-            productContainers[x].appendChild(title);
-            productContainers[x].appendChild(price);
-
-            // Add hover effect
-            productContainers[x].addEventListener('mouseover', function() {
-                this.style.transform = 'scale(1.1)';
-                this.style.backgroundColor = '#F0F0F0'; // Change background color on hover
-                img.src = '/data/images/the-shop-loading-icon.gif';
-                img.src = '/data/images/the_shop_' + z + '_product-image_2.png';
-            });
-
-            productContainers[x].addEventListener('mouseout', function() {
-                this.style.transform = `scale(1)`;
-                this.style.backgroundColor = '#E0E0E0'; // Reset background color when not hovering
+                let img = document.createElement('img')
                 img.src = '/data/images/the_shop_' + z + '_product-image_1.png';
-            });
-            productContainers[x].addEventListener('click', () => {
-              window.location.href = '/dashboard/the-shop/product?code=' + z;
-            });
+
+                let title = document.createElement('p')
+                title.style.fontWeight = 'bold';
+                title.innerHTML = theShopItemNames[z];
+
+                let price = document.createElement('p')
+                price.style.margin = '0 auto';
+                price.innerHTML = '£' + theShopItemPrices[z];
+            
+                // Append elements to the container
+                center.appendChild(img)
+                productContainers[x].appendChild(center);
+                productContainers[x].appendChild(title);
+                productContainers[x].appendChild(price);
+
+                // Add hover effect
+                productContainers[x].addEventListener('mouseover', function() {
+                    this.style.transform = 'scale(1.1)';
+                    this.style.backgroundColor = '#F0F0F0'; // Change background color on hover
+                    img.src = '/data/images/the-shop-loading-icon.gif';
+                    img.src = '/data/images/the_shop_' + z + '_product-image_2.png';
+                });
+
+                productContainers[x].addEventListener('mouseout', function() {
+                    this.style.transform = `scale(1)`;
+                    this.style.backgroundColor = '#E0E0E0'; // Reset background color when not hovering
+                    img.src = '/data/images/the_shop_' + z + '_product-image_1.png';
+                });
+                productContainers[x].addEventListener('click', () => {
+                  window.location.href = '/dashboard/the-shop/product?code=' + z;
+                });
+            }
         }
     } else {
         console.error("No elements with class 'productscontainer' found.");
