cbcfd1fbbbace900af25854f3ae9ce12830a369a
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-11-01 18:17:21 +0000

    Update theShopProductPageScript.js

--- data/scripts/theShopProductPageScript.js
+++ data/scripts/theShopProductPageScript.js
@@ -1,5 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search); const bgh = urlParams.get('code');
 document.addEventListener('DOMContentLoaded', function() { 
+  if (theShopItemNames[bgh]) {
+    continue
+  } else {
+    window.location.replace('/dashboard/the-shop')
   document.getElementById('productImage').src = '/data/images/the_shop_' + bgh + '_product-image_1.png';
   document.getElementById('productTitle').innerHTML = theShopItemNames[bgh];
   document.getElementById('productPrice').innerHTML = '£' + theShopItemPrices[bgh];
