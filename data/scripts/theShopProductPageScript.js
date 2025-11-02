6d8f59c36de441281e8fc47e7e19f1145aa02be1
TheBoysWebsiteV2 <133792673+TheBoysWebsiteV2@users.noreply.github.com>
2025-11-02 17:33:30 +0000

    Update theShopProductPageScript.js

--- data/scripts/theShopProductPageScript.js
+++ data/scripts/theShopProductPageScript.js
@@ -2,13 +2,13 @@ const urlParams = new URLSearchParams(window.location.search); const bgh = urlPa
 document.addEventListener('DOMContentLoaded', function() { 
   if (theShopItemNames[bgh]) {
     console.log(bgh + ' is a valid product.');
-    const data = itemData[bgh]
+    //const data = itemData[bgh]
   } else {
     window.location.replace('/dashboard/the-shop');
   };
   document.getElementById('productImage').src = '/data/images/the_shop_' + bgh + '_product-image_1.png';
-  document.getElementById('productTitle').innerHTML = data[0];
-  document.getElementById('productPrice').innerHTML = '£' + data[1];
+  document.getElementById('productTitle').innerHTML = theShopItemNames[bgh];
+  document.getElementById('productPrice').innerHTML = '£' + theShopItemPrices[bgh];
 });
 function addToCart() {
   if ((sessionStorage.getItem('theShopCart') == null) || (sessionStorage.getItem('theShopCart') == '')) {
